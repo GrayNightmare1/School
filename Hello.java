db668d727b4a9615fb112bf81db46b7b34a77320
GrayNightmare1 <jacob.murray@malad.us>
2026-09-10 16:02:51 -0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello
+ {
+    public static void main(String[] args) 
+    {String name = "Jacob"; int age = 15; String parent1 = "John"; String parent2 = "Sarah"; String sibling1 = "Evan"; String sibling2 = "Logan"; String sibling3 = "Ellie"; String sibling4 = "Mia";
+        System.out.println("Hello World, my name is " + name + " and I am " + age + " years old.");
+        System.out.println("These are my family members:");
+        System.out.println("My parents:");
+        System.out.println(parent1);
+        System.out.println(parent2);
+        System.out.println("My siblings:");
+        System.out.println(sibling1);
+        System.out.println(sibling2);
+        System.out.println(sibling3);
+        System.out.println(sibling4);
+    }
+}
